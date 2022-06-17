bc332de3d92234477a35a2c404bafac4ba15f2f3
Helio Frota <00hf11@gmail.com>
2022-06-17 09:26:17 -0300

    chore: updating namespace

--- tracer.js
+++ tracer.js
@@ -31,9 +31,6 @@ registerInstrumentations({
 });
 
 // Tracer exporter
-// const collectorOptions = {
-//   url: 'http://my-opentel-collector:4318'
-// };
 const traceExporter = new OTLPTraceExporter();
 provider.addSpanProcessor(new BatchSpanProcessor(traceExporter, {
   maxQueueSize: 100,
